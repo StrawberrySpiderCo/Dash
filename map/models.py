0d2b308f41ba87a15cc36423a658155ff3283f95
Anonymous <anonymous@example.com>
2024-05-14 14:04:33 -0700

    added back name and device on net interface

--- map/models.py
+++ map/models.py
@@ -132,7 +132,8 @@ class NetworkInterface(models.Model):
     ipv4_address = models.CharField(max_length=50, null=True, blank=True)
     ipv4_subnet = models.CharField(max_length=50, null=True, blank=True)
     short_name = models.CharField(max_length=17, null=True, blank=True)
-
+    def __str__(self):
+        return f"{self.device} - {self.name}"
     
     
 class NetworkTask(models.Model):
