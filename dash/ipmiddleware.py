0c3b9f6241c876ed9d7de5321c679a5f78ab3641
Anonymous <anonymous@example.com>
2024-05-21 21:32:55 -0700

    Made slight change

--- dash/ipmiddleware.py
+++ dash/ipmiddleware.py
@@ -36,7 +36,7 @@ class LicenseCheckMiddleware:
         try:
             org = Org_Info.objects.get()
             # Check if the organization is set up and the license is not set
-            if org.is_setup and org.organization_license is None:
+            if org.is_setup and org.license == '':
                 return redirect('update_license', org_id=org.org_id)
         except Org_Info.DoesNotExist:
             # If no Org_Info exists, proceed normally
